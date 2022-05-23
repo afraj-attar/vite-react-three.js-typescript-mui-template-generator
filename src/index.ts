0bba8c7054bdfc9e0b0fb4291b7fadca3b195399
Afraj Attar <afraj.attar>
2022-05-23 16:40:25 +0530

    Added logic to debug in vscode Added logic to rename gitignore to .gitignore

--- src/index.ts
+++ src/index.ts
@@ -88,6 +88,9 @@ function createDirectoryContents(templatePath: string, projectPath: string, proj
 
             contents = updateProjectName(contents, projectName, projectChoice); // update project names
 
+            if (file.includes('gitignore'))         // Replace the name of gitignore with .gitignore. Since .gitignore files get's deleted when package is published
+                file = file.replace('gitignore', '.gitignore')
+
             // copy file to destination folder
             const writePath = path.join(currentDir, projectPath, file);
             fs.writeFileSync(writePath, contents, 'utf8');
